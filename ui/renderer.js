15d130844d92bb2df1280736f9c02f039eb2cde0
bhagat-abhishek <inst.abhishek@gmail.com>
2024-10-23 22:45:31 +0530

    inter process communication between application

--- ui/renderer.js
+++ ui/renderer.js
@@ -1,2 +1,9 @@
 const information = document.getElementById("info");
 information.innerText = `You are using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}) and Electron (v${versions.electron()})`;
+
+const func = async () => {
+  const response = await versions.ping();
+  console.log(response);
+};
+
+func();
